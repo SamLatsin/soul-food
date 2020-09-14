9dd64be2284de7db864bd7667dddc9e567d21576
vagon32 <semen.vagon@mail.ru>
2020-09-14 15:17:07 +0300

    12.09

--- app/src/main/java/com/example/soulfood/BasketActivity.java
+++ app/src/main/java/com/example/soulfood/BasketActivity.java
@@ -20,6 +20,9 @@ public class BasketActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private OrderAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    int full_price;
+    int delivery1 = 3;
+    int delivery2 = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +33,14 @@ public class BasketActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         );
-
+        Button pay = (Button)findViewById(R.id.pay);
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BasketActivity.this, SuccessActivity.class);
+                startActivity(intent);
+            }
+        });
         Button back_button = (Button)findViewById(R.id.backButton);
         back_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,9 +48,13 @@ public class BasketActivity extends AppCompatActivity {
                 finish();
             }
         });
+        full_price = getIntent().getExtras().getInt("fullPrice");
+        TextView total_item_text = (TextView)findViewById(R.id.orderPrice);
+        total_item_text.setText("$" + Integer.toString(full_price) + ".00");
+        TextView order_total_price = (TextView)findViewById(R.id.orderFullPrice);
+        order_total_price.setText("$" + Integer.toString(full_price + delivery1 + delivery2) + ".00");
         dishList = (ArrayList<DishItem>) getIntent().getSerializableExtra("dishList");
         dishOrder = new ArrayList<>();
-        
         for(int pos = 0; pos < dishList.size(); pos++) {
             if (dishList.get(pos).getDishCount() != 0) {
                 dishOrder.add(dishList.get(pos));
@@ -71,9 +85,13 @@ public class BasketActivity extends AppCompatActivity {
                 if (!count.getText().toString().equals("0")){
                     dishOrder.get(position).setDishCount(dishOrder.get(position).getDishCount() - 1);
                     count.setText(Integer.toString(dishOrder.get(position).getDishCount()));
-
                     local_price -= dishOrder.get(position).getDishPrice();
                     tv.setText("$" + Integer.toString(local_price));
+                    full_price -= dishOrder.get(position).getDishPrice();
+                    TextView total_item_text = (TextView)findViewById(R.id.orderPrice);
+                    total_item_text.setText("$" + Integer.toString(full_price) + ".00");
+                    TextView order_total_price = (TextView)findViewById(R.id.orderFullPrice);
+                    order_total_price.setText("$" + Integer.toString(full_price + delivery1 + delivery2) + ".00");
 
                 }
             }
@@ -87,6 +105,11 @@ public class BasketActivity extends AppCompatActivity {
                 count.setText(Integer.toString(dishOrder.get(position).getDishCount()));
                 local_price += dishOrder.get(position).getDishPrice();
                 tv.setText("$" + Integer.toString(local_price));
+                full_price += dishOrder.get(position).getDishPrice();
+                TextView total_item_text = (TextView)findViewById(R.id.orderPrice);
+                total_item_text.setText("$" + Integer.toString(full_price) + ".00");
+                TextView order_total_price = (TextView)findViewById(R.id.orderFullPrice);
+                order_total_price.setText("$" + Integer.toString(full_price + delivery1 + delivery2) + ".00");
 
             }
         });
